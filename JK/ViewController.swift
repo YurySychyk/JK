8b4c3ea023574ac2fc7b757b7ac0f0751e9d870a
Yury Sychyk <kauz@tut.by>
2022-02-10 12:14:34 +0300

    create interface and request

--- JK/ViewController.swift
+++ JK/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UILabel!
+    
+    @IBAction func request(_ sender: Any) {
+        makeRequest()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private func makeRequest() {
+        var request = URLRequest(url:URL(string: "https://v2.jokeapi.dev/joke/Any/")!)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            print(String(decoding: data!, as: UTF8.self))
+            print(error)
+            //text.text = String(decoding: data!, as: UTF8.self)
+            }
+        task.resume()
+        
+        
+    }
 }
 
